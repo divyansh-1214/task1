76a7344cc07f6185afee6d3df360091713f7d7d0
divyansh-1214 <divyansh.srri23@gmail.com>
2025-01-06 21:22:38 +0530

    hi

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -62,13 +62,31 @@ function Hero() {
     function handleout6() {
         r6.current.style.transform = "translateX(0px)"
     }
+
+
+    const r21 = useRef(null)
+    const r22 = useRef(null)
+    const r23 = useRef(null)
+    function handlein2(){
+        r21.current.style.transition = "transform 0.8s ease"
+        r21.current.style.transform = "translateY(30px)"
+        r22.current.style.transition = "transform 0.8s ease"
+        r22.current.style.transform = "translateY(-30px)"
+        r23.current.style.transition = "transform 0.8s ease"
+        r23.current.style.transform = "translateY(30px)"
+    }
+    function handleout2(){
+        r21.current.style.transform = "translateY(0px)"
+        r22.current.style.transform = "translateY(0px)"
+        r23.current.style.transform = "translateY(0px)"
+    }
     return (
         <>
             <div className=' grid justify-center items-center  w-screen h-screen overflow-x-auto'>
                 <h1 className='text-7xl pt-10 pb-10 w-full justify-center text-center '>Explore endless <br /> possibilities.</h1>
-                <div className='canvas mx-5 text-center justify-center grid grid-flow-col  grid-cols-4 grid-rows-4 gap-5 md:grid-rows-3 md:grid-cols-7' style={{ height: '50rem', width: '50rem' }}>
-                    <div className=' bg-purple-300 rounded-3xl relative  row-span-2 col-span-2 md:col-span-3' onMouseEnter={handle11} onMouseLeave={handle12}>
-                        <h1 className='flex pt-4 px-4 text-2xl justify-start'>Customization</h1>
+                <div className='canvas mx-5 text-center justify-center grid grid-flow-col   gap-5 grid-rows-3 grid-cols-7' style={{ height: '50rem', width: '50rem' }}>
+                    <div className=' bg-[#c3b2e7] rounded-3xl relative  row-span-2  col-span-3' onMouseEnter={handle11} onMouseLeave={handle12}>
+                        <h1 className='flex pt-4 px-4 text-2xl justify-start text-[#7e4a72]'>Customization</h1>
                         <div className='1 grid grid-rows-3 relative'>
                             <div className='relative '>
                                 <img ref={r11} className='absolute top-12 right-12 ' src="./public/1.2.png" style={{ width: '7.5rem' }} alt="" />
@@ -79,54 +97,54 @@ function Hero() {
                             </div>
                         </div>
                     </div>
-                    <div className=' bg-orange-300 col-span-2 md:col-span-5 rounded-3xl flex overflow-hidden'>
-                        <div className='w-[10rem]'>
-                            <h1>Send Gifts</h1>
-                            <p>Send as a group with friends or individually</p>
+                    <div className=' bg-[#f9a575] relative col-span-5 rounded-3xl flex overflow-hidden'  onMouseEnter={handlein2} onMouseLeave={handleout2}>
+                        <div className='w-[fit] text-[#873915] absolute p-3 bottom-0 text-start'>
+                            <h1 className='text-3xl font-semibold'>Send Gifts</h1>
+                            <p className='text-lg w-[12rem]'>Send as a group with friends or individually</p>
                         </div>
-                        <div className='grid grid-cols-3 w-80   relative ' style={{ transform: 'rotate(-20deg)' }}>
-                            <div className='absolute top-5 left-0 m-5'>
+                        <div className='grid grid-cols-3 w-80 right-[8rem]  absolute ' style={{ transform: 'rotate(-20deg)' }}>
+                            <div className='absolute top-5 left-0 m-5' ref={r21}>
                                 <img className='p-2  w-[8rem] rounded-2xl ' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/6525428301c0c827c6c29243_gifts-card-bath-body-works.webp" alt="" />
                                 <img className='p-2 w-[8rem] rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65254282d3dc76c4aa996868_gifts-card-amazon.webp" alt="" />
                                 <img className='p-2 w-[8rem] rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/652542828b6c40e266484f93_gifts-card-doordash.webp" alt="" />
                             </div>
-                            <div className='absolute top-[-2rem] left-32 px-5'>
+                            <div className='absolute top-[-2rem] left-32 px-5' ref={r22}>
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/652542833f884b05c29eca1a_gifts-card-dunkin-donuts.webp" alt="" />
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce078cb06ac26896fb7_wallet-card-victorias-secret.webp" alt="" />
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce094aa04d73fdff6d4_wallet-card-nike.webp" alt="" />
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65254283c2dced4668a125c8_gifts-card-ulta.webp" alt="" />
                             </div>
-                            <div className='absolute w-64 left-64 px-8'>
+                            <div className='absolute w-64 left-64 px-8' ref={r23}>
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65254283c535bafbfa5b3720_gifts-card-sephora.webp" alt="" />
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65254282816c4f99f9a04c7b_gifts-card-target.webp" alt="" />
                                 <img className='p-2 w-[8rem] top-0 rounded-2xl' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce001c0c827c6bac1fb_wallet-card-starbucks.webp" alt="" />
                             </div>
                         </div>
                     </div>
-                    <div className=' bg-red-300 col-span-4 relative rounded-3xl' onMouseEnter={handle31} onMouseLeave={handle32}>
+                    <div className=' bg-[#f782a4] col-span-4 relative rounded-3xl' onMouseEnter={handle31} onMouseLeave={handle32}>
                         <div className='relative'>
                             <img className='w-36 absolute right-28 top-0 ' ref={r31} style={{ transform: 'rotate(-20deg)' }} src="./public/2.1.png" alt="" />
                             <img className='w-44 absolute right-5 top-0 rounded-xl rotate-12' ref={r32} style={{ transform: 'rotate(20deg)' }} src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253a825d829fa71580e41b_scheduling-ui.svg" alt="" />
                         </div>
-                        <div className='absolute justify-start text-start bottom-0'>
-                            <h1 className='text-lg p-2'>Scheduling</h1>
-                            <p className='w-44 '>Schedule all your cards and gifts now and we’ll send them later</p>
+                        <div className='absolute p-2 justify-start text-start bottom-0 '>
+                            <h1 className='text-3xl font-semibold pb-1 text-[#5f3675]'>Scheduling</h1>
+                            <p className='w-[15rem] text-[#663575]'>Schedule all your cards and gifts now and we’ll send them later</p>
                         </div>
                     </div>
-                    <div className=' bg-green-200 col-span-2 grid grid-rows-2 rounded-3xl' onMouseEnter={handlein4} onMouseLeave={handleout4}>
+                    <div className=' bg-[#c8da8e] col-span-2 grid grid-rows-2 rounded-3xl' onMouseEnter={handlein4} onMouseLeave={handleout4}>
                         <div className='relative justify-center text-center p-2'>
                             <img className='absolute rounded-xl h-20 w-44 top-2 ' ref={r41} style={{ transform: 'rotate(-15deg)' }} src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce078cb06ac26896fb7_wallet-card-victorias-secret.webp" alt="" />
                             <img className='absolute  rounded-xl h-20 w-44 top-4' ref={r42} style={{ transform: 'rotate(-10deg)' }} src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce094aa04d73fdff6d4_wallet-card-nike.webp" alt="" />
                             <img className='absolute  rounded-xl h-20 w-44 top-6 ' ref={r43} style={{ transform: 'rotate(-5deg)' }} src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce001c0c827c6bac1fb_wallet-card-starbucks.webp" alt="" />
                             <img className='absolute  rounded-xl h-20 w-48 top-8' src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253ce030282e8a2f1e202a_wallet-givingli-cash.svg" alt="" />
                         </div>
-                        <div className='justify-start text-start'>
+                        <div className='justify-start text-start text-[#244a1f]'>
                             <h1>Wallet</h1>
                             <p>Access all your gifts and save up your Givingli Cash</p>
                         </div>
                     </div>
-                    <div className=' bg-yellow-300 col-span-2 grid grid-rows-2 px-4 overflow-hidden rounded-3xl' onMouseEnter={handlein5} onMouseLeave={handleout5}>
-                        <div className='justify-start text-start'>
+                    <div className=' bg-[#fede6e] col-span-2 grid grid-rows-2 px-4 overflow-hidden rounded-3xl' onMouseEnter={handlein5} onMouseLeave={handleout5}>
+                        <div className='justify-start text-start text-[#654a1f]'>
                             <h1>Inbox</h1>
                             <p>Track your gifts, group chats, and sent cards</p>
                         </div>
@@ -134,8 +152,8 @@ function Hero() {
                             <img className='rounded-xl w-60' ref={r5} src="https://cdn.prod.website-files.com/65240dac30f703b7a711b7c2/65253fb67d7b8af4623b9bef_inbox-ui.webp" alt="" />
                         </div>
                     </div>
-                    <div className=' bg-gray-400 col-span-2 rounded-3xl grid grid-rows-2 overflow-hidden pl-4' onMouseEnter={handlein6} onMouseLeave={handleout6}>
-                        <div className='w-fit h-fit'>
+                    <div className=' bg-[#b8cfdd] col-span-2 rounded-3xl grid grid-rows-2 overflow-hidden pl-4' onMouseEnter={handlein6} onMouseLeave={handleout6}>
+                        <div className='w-fit h-fit text-[#1a4766]'>
                             <h1>Reminders</h1>
                             <p>Never miss a birthday again</p>
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': { 'max': '390px' },
+        'sm': { 'max': '770px' },
+        'sn': { 'max': '450px' },
+        'so': { 'min': '450px', 'max': '600px' },
+        'md': { 'min': '670px', 'max': '767px' },
+        'rx': { 'min': '600px', 'max': '900px'},
+        'lg': { 'min': '768px', 'max': '1024px' },
+        '1.5xl': { 'min': '1024px', 'max': '1439px'},
+        'xl': { 'min': '1440px' },
+        '2xl': { 'min': '2560px' },
+      },
+    },
   },
   plugins: [],
 }
